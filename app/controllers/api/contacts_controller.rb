00d1e8fb20788294e60c0210dd11605158af6d5b
Sam <Izguerra.sam@gmail.com>
2020-07-06 19:07:48 -0500

    update RESTful contact_app

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -20,4 +20,14 @@ class Api::ContactsController < ApplicationController
   @contact.save
   render 'show.json.jb'
 end
+
+def update
+  @contact = Contact.find_by(id: params[:id])
+  @contact.first_name = params[:first_name]
+  @contact.last_name = params[:last_name]
+  @contact.email = params[:email]
+  @contact.phone_number = params[:phone_number]
+  @contact.save
+  render 'show.json.jb'
+ end
 end
