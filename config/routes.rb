8df3c26c044537deb2611bf79d08a2143ffcee70
Sam <Izguerra.sam@gmail.com>
2020-07-06 19:02:21 -0500

    index/show/create for contact_app

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  
+  
+  namespace :api do
+    get "/contacts" => "contacts#index"
+    get "/contacts/:id" => "contacts#show"
+    post "/contacts" => "contacts#create"
+  
+  
+  end
 end
