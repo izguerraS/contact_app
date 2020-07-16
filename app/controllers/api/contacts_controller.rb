bc02cd6cf545abb9bb6b9424c7f60e3ae7674178
Sam <Izguerra.sam@gmail.com>
2020-07-16 16:27:49 -0500

    contacts app up to date

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,40 +1,37 @@
 class Api::ContactsController < ApplicationController
-
   def index
     @contacts = Contact.all
-    render 'index.json.jb'
+    render "index.json.jb"
   end
-
   def show
     @contact = Contact.find_by(id: params[:id])
     render 'show.json.jb'
- end
-
- def create
-  @contact = Contact.new(
-    first_name: params[:first_name],
-    last_name: params[:last_name],
-    email: params[:email],
-    phone_number: params[:phone_number],
-  )
-  @contact.save
-  render 'show.json.jb'
-end
-
-def update
-  @contact = Contact.find_by(id: params[:id])
-  @contact.first_name = params[:first_name]
-  @contact.last_name = params[:last_name]
-  @contact.email = params[:email]
-  @contact.phone_number = params[:phone_number]
-  @contact.save
-  render 'show.json.jb'
- end
-
- def destroy
-  @contact = Contact.find_by(id: params[:id])
-  @contact.destroy
-render 'destroy.json.jb'
-
- end
+  end
+  def create
+    @contact = Contact.create(      
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      user_id: current_user.id
+    )
+    render 'show.json.jb'
+  end
+  
+  def update
+    # find the correct contact
+    @contact = Contact.find_by(id: params[:id])
+    # update that particular contact
+    @contact.update(
+      first_name: params[:first_name] || @contact.first_name,
+      last_name: params[:last_name] || @contact.last_name,
+      phone_number: params[:phone_number] || @contact.phone_number,
+      email: params[:email] || @contact.email
+    )
+    render 'show.json.jb'
+  end
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    render json: {message: "contact destroyed"}
+  end
 end
